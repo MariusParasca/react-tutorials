0ac36ff46b2e82bd6644615a4480e47dd77f457b
mariusjack <mrs.parasca@gmail.com>
2023-05-19 09:53:23 +0300

    modern dialog modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
+import { useState } from 'react';
+import { Button } from './components/Button/Button';
 import { Wrapper } from './components/Wrapper/Wrapper';
+import { Modal } from './components/Modal/Modal';
 
 function App() {
+  const [openModal, setOpenModal] = useState(false);
+
   return (
-    <Wrapper>
-      <div></div>
-    </Wrapper>
+    <>
+      <Wrapper>
+        <div>
+          <Button onClick={() => setOpenModal(true)}>Open Modal</Button>
+        </div>
+      </Wrapper>
+      <Modal
+        open={openModal}
+        onClose={() => {
+          console.log('%c It closes', 'color: red');
+          setOpenModal(false);
+        }}
+      >
+        <p>Some text</p>
+        <Button onClick={() => setOpenModal(false)}>Close Modal</Button>
+      </Modal>
+    </>
   );
 }
 
