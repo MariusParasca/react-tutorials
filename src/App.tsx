4708336807c1185019e007cbac9f22ad22ba4504
mariusjack <mrs.parasca@gmail.com>
2023-05-19 10:00:15 +0300

    clean up master

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,13 @@
-import { useState } from 'react';
 import { Button } from './components/Button/Button';
 import { Wrapper } from './components/Wrapper/Wrapper';
-import { Modal } from './components/Modal/Modal';
 
 function App() {
-  const [openModal, setOpenModal] = useState(false);
-
   return (
-    <>
-      <Wrapper>
-        <div>
-          <Button onClick={() => setOpenModal(true)}>Open Modal</Button>
-        </div>
-      </Wrapper>
-      <Modal
-        open={openModal}
-        onClose={() => {
-          console.log('%c It closes', 'color: red');
-          setOpenModal(false);
-        }}
-      >
-        <p>Some text</p>
-        <Button onClick={() => setOpenModal(false)}>Close Modal</Button>
-      </Modal>
-    </>
+    <Wrapper>
+      <div>
+        <Button>Hey</Button>
+      </div>
+    </Wrapper>
   );
 }
 
